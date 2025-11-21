1f9442c458b3c0e41a88d3d79f71d70e5e3ad240
NRavuri <nikhilravuri.r@outlook.com>
2025-11-21 16:50:59 +0000

    Initial OpenShift project files

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("BookStore API is running!"));
+app.listen(8080, () => console.log("Server running on port 8080"));
+
